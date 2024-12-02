366967f40127d9f13c39af4b8cb982d68dfff00b
Jiye Su <1915599811@qq.com>
2024-12-02 20:35:00 +0800

    Add files via upload

---
+++ AIOJ-1/04.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    double a = 0;
+    cin >> a;
+
+    if ((a >= 100) && ( a <= 400))
+    {
+        cout << "yes" << endl;
+    }
+
+    else
+    {
+        cout << "no" << endl;
+    }
+
+    return 0;
+}
